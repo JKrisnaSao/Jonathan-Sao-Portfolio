c2caf61e50127728966816c04fd967599a49f7c7
Jonathan Sao <124708158+JKrisnaSao@users.noreply.github.com>
2024-07-18 01:15:08 -0400

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,7 @@ const config: GatsbyConfig = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
     siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Jonathan Sao's Portfolio`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
